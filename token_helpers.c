f1a3ca0198f26b7e7d9c2ec173fa325b8d72827a
Derrick Ojo-Igbinoba <88897191+Osamede1997@users.noreply.github.com>
2022-11-17 02:40:24 -0800

    Update token_helpers.c

--- token_helpers.c
+++ token_helpers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * tokenize - creates array of tokens based on user string
+ * _createtoken - creates array of tokens based on user string
  * @buffer: pointer to user string
  *
  * Return: pointer to array of user strings
